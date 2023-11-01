e12ed2e81132c553e14d707e27a911cef5f3ee19
EsterDahri <esterdahri@gmail.com>
2023-11-01 22:55:06 +0100

    populate seeder + create model and controller

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -5,13 +5,48 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Train;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
 class TrainsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        $agencies = [
+            'Trenitalia',
+            'SNCF (Société Nationale des Chemins de Fer Français)',
+            'Deutsche Bahn',
+            'Amtrak',
+            'JR East (East Japan Railway Company)',
+            'Renfe Operadora',
+            'China Railway Corporation',
+            'Indian Railways',
+            'Russian Railways',
+            'Canadian National Railway'
+        ];
+
+        for ($i = 0; $i < 30; $i++) {
+            $new_train = new Train();
+            $new_train->agency = $faker->randomElement($agencies);
+            $new_train->departure_station = $faker->word();
+            $new_train->arrival_station = $faker->word();
+            $new_train->departure_time = $faker->time();
+            $new_train->arrival_time = $faker->time();
+            $new_train->train_code = implode('', [$faker->randomLetter(), $faker->randomLetter(), $faker->randomNumber(3)]);
+            $new_train->number_of_carriages = $faker->randomDigitNotNull();
+            $new_train->in_time = $faker->boolean();
+            $new_train->deleted = $faker->boolean();
+            $new_train->slug = Str::slug(implode(' ', [$new_train->departure_station, $new_train->arrival_station,  $new_train->train_code]), '-');
+
+            //! PER FARE UN CONTROLLO  dump($new_train);  + nel terminale: php artisan db:seed --class=TrainsTableSeeder
+            // dump($new_train);
+
+            //* per inviarli al database $new_train->save(); + nel terminale: php artisan db:seed --class=TrainsTableSeeder
+            $new_train->save();
+        }
     }
 }
