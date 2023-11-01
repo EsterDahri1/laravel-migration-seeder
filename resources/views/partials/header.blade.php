5cc0ba052d8f0306c4374ad1c7190a7da7999ef6
EsterDahri <esterdahri@gmail.com>
2023-11-01 23:49:57 +0100

    text white on a nav

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header class="bg-dark text-center text-white">
-    <nav class="nav justify-content-center  ">
-        <a class="nav-link active" href="{{ route('home') }}">Home</a>
-        <a class="nav-link" href="#">About</a>
-        <a class="nav-link" href="#">Contacts</a>
+    <nav class="nav justify-content-center text-white">
+        <a class="nav-link active text-white" href="{{ route('home') }}">Home</a>
+        <a class="nav-link text-white" href="#">About</a>
+        <a class="nav-link text-white" href="#">Contacts</a>
     </nav>
 </header>
