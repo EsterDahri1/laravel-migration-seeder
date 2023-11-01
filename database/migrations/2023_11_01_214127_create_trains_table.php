4bd2dd89991df42f1165e764aeb824689eab4b15
EsterDahri <esterdahri@gmail.com>
2023-11-01 22:44:08 +0100

    add table columns

--- database/migrations/2023_11_01_214127_create_trains_table.php
+++ database/migrations/2023_11_01_214127_create_trains_table.php
@@ -14,6 +14,17 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
+            $table->string('agency', 60);
+            $table->string('departure_station', 30);
+            $table->string('arrival_station', 30);
+            $table->string('departure_time', 8);
+            $table->string('arrival_time', 8);
+            $table->string('train_code', 5)->unique();
+            $table->tinyInteger('number_of_carriages')->unsigned()->default(0);
+            $table->boolean('in_time')->nullable();
+            $table->boolean('deleted')->nullable();
+            $table->string('slug', 50)->unique();
         });
     }
 
